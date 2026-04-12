5d9179cc99352dd3ed51c88b2471e7c114e00239
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2026-04-12 02:33:54 +0200

    fix(person-endpoints): incorrect returned id value and invalid service registration

--- src/TUnit.Sample.ApiService/Endpoints/Persons/PersonsEndpoints.cs
+++ src/TUnit.Sample.ApiService/Endpoints/Persons/PersonsEndpoints.cs
@@ -21,46 +21,48 @@ public static class PersonsEndpoints
     {
         var group = app.MapGroup("/persons");
 
-        group.MapGet("/", async (IPersonService personService, CancellationToken ct)
+        group.MapGet("/", async ([FromServices] IPersonService personService, CancellationToken ct)
             => Results.Ok(await personService.GetAll(ct))
         );
 
-        group.MapGet("/{id:guid}", async (Guid id, PersonService personService, CancellationToken ct) => {
+        group.MapGet("/{id:guid}", async (Guid id, [FromServices] IPersonService personService, CancellationToken ct) => {
             var person = await personService.GetById(id, ct);
             return person is null
                 ? Results.NotFound()
                 : Results.Ok(person);
         });
 
-        group.MapPost("/", async (CreatePersonRequest request, IPersonService personService, CancellationToken ct) =>
+        group.MapPost("/", async ([FromBody] CreatePersonRequest request, [FromServices] IPersonService personService, CancellationToken ct) =>
             await personService.Insert(request, ct) is {} id
-                ? Results.Created($"/persons/{id}", (object?)id)
+                ? Results.Created($"/persons/{id}", id)
                 : Results.BadRequest()
         );
 
-        group.MapPatch("/{id:guid}", async (Guid id, [FromBody] UpdatePersonRequest request, IPersonService personService, CancellationToken ct)
+        group.MapPatch("/{id:guid}", async (Guid id, [FromBody] UpdatePersonRequest request, [FromServices] IPersonService personService, CancellationToken ct)
             => await personService.Update(request, id, ct)
                 ? Results.NoContent()
                 : Results.NotFound()
         );
 
-        group.MapPut("/{id:guid}", async (Guid id, [FromBody] UpdatePersonRequest request, IPersonService personService, CancellationToken ct) => {
+        group.MapPut("/{id:guid}", async (Guid id, [FromBody] UpdatePersonRequest request, [FromServices] IPersonService personService, CancellationToken ct) => {
             var updated = await personService.Update(request, id, ct);
             if (updated)
                 return Results.NoContent();
 
+            // TODO: specify FluentValidation rules instead 
             if (!request.BirthDate.HasValue
                 || string.IsNullOrWhiteSpace(request.FirstName)
                 || string.IsNullOrWhiteSpace(request.LastName))
                 return Results.BadRequest("Person could not be found or invalid data provided.");
 
             var createRequest = new CreatePersonRequest(request.FirstName, request.LastName, request.BirthDate.Value);
-            await personService.Insert(createRequest, ct);
 
-            return Results.Created($"/persons/{id}", id);
+            return await personService.Insert(createRequest, ct) is {} createdId
+                ? Results.Created($"/persons/{createdId}", createdId)
+                : Results.BadRequest();
         });
 
-        group.MapDelete("/{id:guid}", async (Guid id, IPersonService personService, CancellationToken ct)
+        group.MapDelete("/{id:guid}", async (Guid id, [FromServices] IPersonService personService, CancellationToken ct)
             => await personService.Delete(id, ct)
                 ? Results.NoContent()
                 : Results.NotFound()
