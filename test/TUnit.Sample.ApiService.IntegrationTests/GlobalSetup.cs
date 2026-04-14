35e065d7bbec8a2d691a620c7b9ca030f7d8b468
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2026-04-14 18:36:42 +0200

    test: add global category markers to separate the test suites

--- test/TUnit.Sample.ApiService.IntegrationTests/GlobalSetup.cs
+++ test/TUnit.Sample.ApiService.IntegrationTests/GlobalSetup.cs
@@ -2,3 +2,4 @@
 
 // You can use attributes at the assembly level to apply to all tests in the assembly
 [assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[assembly: Category("Integration")]
--- test/TUnit.Sample.Web.Tests/GlobalSetup.cs
+++ test/TUnit.Sample.Web.Tests/GlobalSetup.cs
@@ -1,1 +1,2 @@
 ﻿[assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[assembly: Category("Components")]
