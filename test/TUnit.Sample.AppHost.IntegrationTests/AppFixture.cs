778d8d53d40dbc92117a2f8fd189c31f746fe2e8
Mark Novak <67005577+tavanuka@users.noreply.github.com>
2026-04-14 19:47:44 +0200

    test(aspire): wait for only resources under test to spin up

--- test/TUnit.Sample.AppHost.IntegrationTests/AppFixture.cs
+++ test/TUnit.Sample.AppHost.IntegrationTests/AppFixture.cs
@@ -7,7 +7,7 @@ public class AppFixture : AspireFixture<Projects.TUnit_Sample_AppHost>
 {
     protected override TimeSpan ResourceTimeout => TimeSpan.FromMinutes(3);
     protected override ResourceWaitBehavior WaitBehavior => ResourceWaitBehavior.Named;
-    protected override IEnumerable<string> ResourcesToWaitFor() => [ResourceConstants.WebApi, ResourceConstants.WebFrontend];
+    protected override IEnumerable<string> ResourcesToWaitFor() => [ResourceConstants.WebApi];
     
     protected override void ConfigureBuilder(IDistributedApplicationTestingBuilder builder)
     {
